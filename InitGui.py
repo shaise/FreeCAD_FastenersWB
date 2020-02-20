472b9b95a783b0e31aacce7aafc52ddbc0f55f0b
Shai Seger <shaise@gmail.com>
2020-02-20 08:58:48 +0200

    Add DIN 7984 screws - update version

--- InitGui.py
+++ InitGui.py
@@ -30,7 +30,7 @@ fnWB_icons_path =  os.path.join( fnWBpath, 'Icons')
 global main_fnWB_Icon
 main_fnWB_Icon = os.path.join( fnWB_icons_path , 'FNLogo.svg')
 
-FASTENERSWB_VERSION = 'V0.3.12'
+FASTENERSWB_VERSION = 'V0.3.13'
 
 class FastenersWorkbench (Workbench):
  

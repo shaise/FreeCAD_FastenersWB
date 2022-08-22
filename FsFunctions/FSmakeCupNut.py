d45dd95d7934a7647d5d43f8d040d0488673ebb8
M G Berberich <github@oss.m-berberich.de>
2022-08-22 19:41:41 +0200

    fixes no-hole in DIN 1587
    fixes problem #1 in issue #163
    also changes outer form slightly (no step)

--- FsFunctions/FSmakeCupNut.py
+++ FsFunctions/FSmakeCupNut.py
@@ -33,9 +33,11 @@ def makeCupNut(self, fa): # dynamically loaded method of class Screw
     SType = fa.type
     dia = self.getDia(fa.calc_diam, True)
     if SType == "DIN1587":
-        P, d_k, h, m, s, t = fa.dimTable
+        P, d_k, h, m, s, t, w = fa.dimTable
     else:
         raise RuntimeError("unknown screw type")
+    sq3 = math.sqrt(3)
+    ec = (2 - sq3) * s / 6
     pnts = list(
         map(
             lambda x: Base.Vector(x),
@@ -43,14 +45,13 @@ def makeCupNut(self, fa): # dynamically loaded method of class Screw
                 [0, 0, 1.1 * dia / 4],
                 [1.1 * dia / 2, 0, 0],
                 [s / 2, 0, 0],
-                [s * math.sqrt(3) / 3, 0, 0.045 * s],
-                [s * math.sqrt(3) / 3, 0, m - 0.045 * s],
-                [s / 2, 0, m],
-                [d_k / 2, 0, m],
-                [d_k / 2, 0, h - d_k / 2],
+                [s * sq3 / 3, 0, ec],
+                [s * sq3 / 3, 0, m - ec],
+                [d_k / 2, 0, (m - ec) + (2 * s - sq3 * d_k) / 6 ],
+                [d_k / 2, 0, h - d_k / 2 ],
                 [d_k / 2 * math.sqrt(2) / 2, 0, h - d_k / 2 + d_k / 2 * math.sqrt(2) / 2],
-                [0, 0, h],
-            ],
+                [0, 0, h]
+            ]
         )
     )
     profile = Part.Wire(
@@ -61,9 +62,8 @@ def makeCupNut(self, fa): # dynamically loaded method of class Screw
             Part.makeLine(pnts[3], pnts[4]),
             Part.makeLine(pnts[4], pnts[5]),
             Part.makeLine(pnts[5], pnts[6]),
-            Part.makeLine(pnts[6], pnts[7]),
-            Part.Arc(pnts[7], pnts[8], pnts[9]).toShape(),
-            Part.makeLine(pnts[9], pnts[0]),
+            Part.Arc(pnts[6], pnts[7], pnts[8]).toShape(),
+            Part.makeLine(pnts[8], pnts[0])
         ]
     )
     shell = profile.revolve(Base.Vector(0, 0, 0), Base.Vector(0, 0, 1), 360)
@@ -114,4 +114,20 @@ def makeCupNut(self, fa): # dynamically loaded method of class Screw
         thread_chamfer = Part.Solid(cham_shell)
         tap_tool = tap_tool.cut(thread_chamfer)
         solid = solid.cut(tap_tool)
+    else:
+        hole_pts = [ Base.Vector(x, 0, y) for x, y in [
+            [0, 0],
+            [0, h-w],
+            [dia/2.0, t],
+            [dia/2.0, 0]
+        ] ]
+        hole_profile = Part.Wire( [
+            Part.makeLine(hole_pts[0], hole_pts[1]),
+            Part.makeLine(hole_pts[1], hole_pts[2]),
+            Part.makeLine(hole_pts[2], hole_pts[3]),
+            Part.makeLine(hole_pts[3], hole_pts[0])
+        ] )
+        hole = Part.Solid(hole_profile.revolve(
+            Base.Vector(0, 0, 0), Base.Vector(0, 0, 1), 360))
+        solid = solid.cut(hole)
     return solid

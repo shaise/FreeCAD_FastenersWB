9c99a438b6fdc6fa151fad313e95e3ab38f2c244
Henrique Enzweiler <henriqueenzweiler@gmail.com>
2022-09-16 09:50:08 -0300

    Update pt-br translations

--- translations/fasteners_pt-br.ts
+++ translations/fasteners_pt-br.ts
@@ -4,37 +4,37 @@
 <context>
     <name>DlgChangeParams</name>
     <message>
-        <location filename="../FSChangeParams.py" line="95"/>
+        <location filename="../FSChangeParams.py" line="99"/>
         <source>Change fastener parameters</source>
         <translation>Alterar parâmetros dos fixadores</translation>
     </message>
     <message>
-        <location filename="../FSChangeParams.py" line="96"/>
+        <location filename="../FSChangeParams.py" line="100"/>
         <source>Fastener Parameters</source>
         <translation>Parâmetros dos fixadores</translation>
     </message>
     <message>
-        <location filename="../FSChangeParams.py" line="97"/>
+        <location filename="../FSChangeParams.py" line="101"/>
         <source>Fastener type:</source>
         <translation>Tipo de fixador:</translation>
     </message>
     <message>
-        <location filename="../FSChangeParams.py" line="98"/>
+        <location filename="../FSChangeParams.py" line="102"/>
         <source>Auto set diameter</source>
         <translation>Definição automática de diâmetro</translation>
     </message>
     <message>
-        <location filename="../FSChangeParams.py" line="99"/>
+        <location filename="../FSChangeParams.py" line="103"/>
         <source>Diameter:</source>
         <translation>Diâmetro:</translation>
     </message>
     <message>
-        <location filename="../FSChangeParams.py" line="100"/>
+        <location filename="../FSChangeParams.py" line="104"/>
         <source>Length:</source>
         <translation>Comprimento:</translation>
     </message>
     <message>
-        <location filename="../FSChangeParams.py" line="101"/>
+        <location filename="../FSChangeParams.py" line="105"/>
         <source>Set length (mm):</source>
         <translation>Defina o comprimento (mm):</translation>
     </message>
@@ -44,101 +44,184 @@
     <message>
         <location filename="../CountersunkHoles.py" line="121"/>
         <source>Countersunk screw holes</source>
-        <translation type="unfinished"></translation>
+        <translation>Furos escareados para parafuso</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="122"/>
         <source>Shape</source>
-        <translation type="unfinished"></translation>
+        <translation>Formato</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="123"/>
         <source>Base shape:</source>
-        <translation type="unfinished"></translation>
+        <translation>Formato base:</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="124"/>
         <source>Base</source>
-        <translation type="unfinished"></translation>
+        <translation>Base</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="125"/>
         <source>Chamfer Parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>Parâmetros do chanfro</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="126"/>
         <source>All</source>
-        <translation type="unfinished"></translation>
+        <translation>Todos</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="127"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Nenhum</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="128"/>
         <source>Diameter:</source>
-        <translation type="unfinished"></translation>
+        <translation>Diâmetro:</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="129"/>
         <source>No selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Nada selecionado</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="130"/>
         <source>Screw type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Tipo de rosca:</translation>
     </message>
     <message>
         <location filename="../CountersunkHoles.py" line="131"/>
         <source>No Selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Nada selecionado</translation>
     </message>
 </context>
 <context>
     <name>DockWidget</name>
     <message>
-        <location filename="../FSScrewCalc.py" line="101"/>
+        <location filename="../FSScrewCalc.py" line="105"/>
         <source>Screw hole calculator</source>
         <translation>Calculadora de furo de parafuso</translation>
     </message>
     <message>
-        <location filename="../FSScrewCalc.py" line="102"/>
+        <location filename="../FSScrewCalc.py" line="106"/>
         <source>Fastener type:</source>
         <translation>Tipo de fixador:</translation>
     </message>
     <message>
-        <location filename="../FSScrewCalc.py" line="103"/>
+        <location filename="../FSScrewCalc.py" line="107"/>
         <source>Screw Diameter:</source>
         <translation>Diametro da rosca:</translation>
     </message>
     <message>
-        <location filename="../FSScrewCalc.py" line="104"/>
+        <location filename="../FSScrewCalc.py" line="108"/>
         <source>Suggested Hole diameter (mm):</source>
         <translation>Diametro de furo sugerido (mm):</translation>
     </message>
 </context>
 <context>
+    <name>FastenerBase</name>
+    <message>
+        <location filename="../FastenerBase.py" line="636"/>
+        <source>Invert fastener</source>
+        <translation>Inverter elemento fixador</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="637"/>
+        <source>Invert fastener orientation</source>
+        <translation>Inverte o sentido do elemento fixador</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="676"/>
+        <source>Move fastener</source>
+        <translation>Mover elemento fixador</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="677"/>
+        <source>Move fastener to a new location</source>
+        <translation>Move o elemento fixador para um novo local</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="43"/>
+        <source>Match for pass hole</source>
+        <translation>Acertar pelo diâmetro externo</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="45"/>
+        <source>Match for tap hole</source>
+        <translation>Acertar pelo diâmetro interno</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="718"/>
+        <source>Simplify shape</source>
+        <translation>Simplificar formato</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="719"/>
+        <source>Change object to simple non-parametric shape</source>
+        <translation>Alterar objeto para forma não paramétrica simples</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="752"/>
+        <source>Toggle Match Type</source>
+        <translation>Alternar o tipo de acerto</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="758"/>
+        <source>Toggle auto screw diameter matching inner&lt;-&gt;outer thread</source>
+        <translation>Alternar entre interno&lt;-&gt;externo no acertador de diâmetro automático</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="799"/>
+        <source>Match screws by inner thread diameter (Tap hole)</source>
+        <translation>Acertar os parafusos pelo diâmetro interno da rosca</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="815"/>
+        <source>Match screws by outer thread diameter (Pass hole)</source>
+        <translation>Acertar os parafusos pelo diâmetro externo da rosca</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="836"/>
+        <source>Screw diameter matching mode</source>
+        <translation>Modo para acertar o diâmetro do parafuso</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="837"/>
+        <source>Screw diameter matching mode (by inner or outer thread diameter)</source>
+        <translation>Modo do acertador automático de parafuso (pelo diâmetro interno ou externo)</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="866"/>
+        <source>Generate BOM</source>
+        <translation>Gerar lista de materiais</translation>
+    </message>
+    <message>
+        <location filename="../FastenerBase.py" line="867"/>
+        <source>Generate fasteners bill of material</source>
+        <translation>Gerar lista de materiais de elementos fixadores</translation>
+    </message>
+</context>
+<context>
     <name>InitGui</name>
     <message>
-        <location filename="../InitGui.py" line="56"/>
+        <location filename="../InitGui.py" line="49"/>
         <source>Translate this text</source>
         <translation>Traduza este texto</translation>
     </message>
     <message>
-        <location filename="../InitGui.py" line="73"/>
+        <location filename="../InitGui.py" line="66"/>
         <source>Fasteners</source>
-        <translation>Fixadores</translation>
+        <translation>Elementos fixadores</translation>
     </message>
     <message>
-        <location filename="../InitGui.py" line="70"/>
+        <location filename="../InitGui.py" line="63"/>
         <source>Add </source>
         <translation>Adicionar </translation>
     </message>
     <message>
-        <location filename="../InitGui.py" line="73"/>
+        <location filename="../InitGui.py" line="66"/>
         <source>Add Fasteners</source>
         <translation>Adicionar fixadores</translation>
     </message>

d90c268ffc83c9a6e9e309e686faf321bbb299f3
FreeCAD-Tools <90561109+FreeCAD-Tools@users.noreply.github.com>
2022-10-14 18:21:03 +0300

    Minor fix update.sh

--- translations/update.sh
+++ translations/update.sh
@@ -10,13 +10,12 @@
 # sudo apt install -y pyqt5-dev-tools
 #
 # This is array of supported languages.
-# When adding new  languages, this list needs to be update.
+# When adding new languages, this list needs to be update.
 languages=(es-ar es-es es-mx pt-br pt-pt ru)
 for lang in ${languages[*]}
 do
    # Creation of uifiles.ts file from ../*.ui files with designation of language code
    lupdate ../*.ui -ts uifiles.ts -source-language en_US -target-language $lang
-   # lupdate ../*.ui -ts uifiles.ts
    # Creation of pyfiles.ts file from ../*.py files
    pylupdate5 ../*.py -ts pyfiles.ts -verbose
    # Join uifiles.ts and pyfiles.ts files to fasteners.ts

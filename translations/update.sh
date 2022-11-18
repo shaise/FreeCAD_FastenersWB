c90580165fcb067c9d7e85b74273199835936cfd
FreeCAD-Tools <90561109+FreeCAD-Tools@users.noreply.github.com>
2022-11-18 21:06:24 +0300

    Updating the article about translation
    * Update sh script.
    * Update article content.
    * Add images.
    * Rename HOW_TO_ADD_NEW_LANGUAGE.md to TRANSLATIONS.md and move it to main folder.
    * Add link to README.md file.

--- translations/update.sh
+++ translations/update.sh
@@ -12,18 +12,33 @@
 languages=(es-ar es-es pt-br pt-pt ru)
 for lang in ${languages[*]}
 do
-   # Creation of uifiles.ts file from ../*.ui files with designation of language code ( -no-obsolete)
-   lupdate ../*.ui -ts uifiles.ts -source-language en_US -target-language $lang
-   # Creation of pyfiles.ts file from ../*.py files
-   pylupdate5 ../*.py -ts pyfiles.ts -verbose
-   # Join uifiles.ts and pyfiles.ts files to fasteners.ts
-   lconvert -i uifiles.ts pyfiles.ts -o fasteners.ts
-   # Join fasteners.ts to exist fasteners_(language).ts file ( -no-obsolete)
-   lconvert -i fasteners.ts fasteners_$lang.ts -o fasteners_$lang.ts
-   # (Release) Creation of *.qm file from fasteners_(language).ts
-   lrelease fasteners_$lang.ts
-   # Delete unused files
-   rm uifiles.ts
-   rm pyfiles.ts
-   rm fasteners.ts
+   # Check if fastners_$lang.ts exist
+   if [ -f "fasteners_$lang.ts" ]; then
+      echo -e '\033[1;32m\n     <<< Update translation for '$lang' language >>> \n\033[m';
+      # Creation of uifiles.ts file from ../*.ui files with designation of language code
+      lupdate ../*.ui -ts uifiles.ts -source-language en_US -target-language $lang -no-obsolete
+      # Creation of pyfiles.ts file from ../*.py files
+      pylupdate5 ../*.py -ts pyfiles.ts -verbose
+      # Join uifiles.ts and pyfiles.ts files to fasteners.ts
+      lconvert -i uifiles.ts pyfiles.ts -o fasteners.ts
+      # Join fasteners.ts to exist fasteners_(language).ts file ( -no-obsolete)
+      lconvert -i fasteners.ts fasteners_$lang.ts -o fasteners_$lang.ts
+      # (Release) Creation of *.qm file from fasteners_(language).ts
+      lrelease fasteners_$lang.ts
+      # Delete unused files
+      rm uifiles.ts
+      rm pyfiles.ts
+      rm fasteners.ts
+   else
+      echo -e '\033[1;33m\n     <<< Create files for added '$lang' language >>> \n\033[m';
+      # Creation of uifiles.ts file from ../*.ui files with designation of language code
+      lupdate ../*.ui -ts uifiles.ts -source-language en_US -target-language $lang -no-obsolete
+      # Creation of pyfiles.ts file from ../*.py files
+      pylupdate5 ../*.py -ts pyfiles.ts -verbose
+      # Join uifiles.ts and pyfiles.ts files to fasteners_$lang.ts
+      lconvert -i uifiles.ts pyfiles.ts -o fasteners_$lang.ts
+      # Delete unused files
+      rm uifiles.ts
+      rm pyfiles.ts
+   fi
 done

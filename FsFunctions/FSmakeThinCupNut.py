bd40ec1639f73b8a7a03ed6e0648e5af3b06f852
alexneufeld <alex.d.neufeld@gmail.com>
2022-12-10 21:31:54 -0700

    FSmakeThinCupNut: simplify code
    make profile with FSFaceMaker and use CreateBlindInnerThreadCutter to
    cut threads

--- FsFunctions/FSmakeThinCupNut.py
+++ FsFunctions/FSmakeThinCupNut.py
@@ -26,50 +26,46 @@
 ***************************************************************************
 """
 from screw_maker import *
+import FastenerBase
 
-# DIN917 Cap nuts, thin style
 
-def makeThinCupNut(self, fa): # dynamically loaded method of class Screw
+def makeThinCupNut(self, fa):
+    """DIN917 Cap nuts, thin style"""
     dia = self.getDia(fa.calc_diam, True)
     P, g2, h, r, s, t, w = fa.dimTable
 
-    H = P * math.cos(math.radians(30)) * 5.0 / 8.0 # Gewindetiefe H
+    H = P * math.cos(math.radians(30)) * 5.0 / 8.0
     if fa.thread: H *= 1.1
     e = s / math.sqrt(3) * 2.0
     cham_i = H * math.tan(math.radians(15.0))
     cham_o = (e - s) * math.tan(math.radians(15.0))
     d = dia / 2.0
-        
-    Pnt0 = Base.Vector(d - H, 0.0, cham_i)
-    Pnt1 = Base.Vector(d, 0.0, 0.0)
-    Pnt2 = Base.Vector(s / 2.0, 0.0, 0.0)
-    Pnt3 = Base.Vector(e / 2.0, 0.0, cham_o)
-    Pnt4 = Base.Vector(e / 2.0, 0.0, h - r + math.sqrt(r * r - e * e / 4.0))
-    Pnt5 = Base.Vector(e / 4.0, 0.0, h - r + math.sqrt(r * r - e * e / 16.0))
-    Pnt6 = Base.Vector(0.0, 0.0, h)
-    Pnt7 = Base.Vector(0.0, 0.0, h-w)
-    Pnt8 = Base.Vector(d - H, 0.0, t)
 
-    edge0 = Part.makeLine(Pnt0, Pnt1)
-    edge1 = Part.makeLine(Pnt1, Pnt2)
-    edge2 = Part.makeLine(Pnt2, Pnt3)
-    edge3 = Part.makeLine(Pnt3, Pnt4)
-    edge4 = Part.Arc(Pnt4, Pnt5, Pnt6).toShape()
-    edge5 = Part.makeLine(Pnt6, Pnt7)
-    edge6 = Part.makeLine(Pnt7, Pnt8)
-    edge7 = Part.makeLine(Pnt8, Pnt0)
-
-    aWire = Part.Wire([edge0, edge1, edge2, edge3, edge4, edge5, edge6, edge7])
-    # Part.show(aWire)
-    aFace = Part.Face(aWire)
-    head = self.RevolveZ(aFace)
+    fm = FastenerBase.FSFaceMaker()
+    fm.AddPoint(d, 0.0)
+    fm.AddPoint(s / 2.0, 0.0)
+    fm.AddPoint(e / 2.0, cham_o)
+    fm.AddPoint(e / 2.0, h - r + math.sqrt(r * r - e * e / 4.0))
+    fm.AddArc(
+        e / 4.0,
+        h - r + math.sqrt(r * r - e * e / 16.0),
+        0.0,
+        h
+    )
+    fm.AddPoint(0.0, h-w)
+    fm.AddPoint(0.0, d * math.tan(math.radians(15)))
+    head = self.RevolveZ(fm.GetFace())
     extrude = self.makeHextool(s, h, s * 2.0)
     nut = head.cut(extrude)
 
     if fa.thread:
-        turns = int(math.floor(t/P))
-        threadCutter = self.makeInnerThread_2(dia, P, turns, None, t)
-        threadCutter.translate(Base.Vector(0.0, 0.0, turns * P))
-        nut = nut.cut(threadCutter)
-
+        threadCutter = self.CreateBlindInnerThreadCutter(dia, P, t - 1.5 * P)
+    else:
+        fm.Reset()
+        fm.AddPoint(0.0, 0.0)
+        fm.AddPoint(d - H, 0.0)
+        fm.AddPoint(d - H, t - P)
+        fm.AddPoint(0.0, t - P + (d - H) / math.tan(math.radians(59)))
+        threadCutter = self.RevolveZ(fm.GetFace())
+    nut = nut.cut(threadCutter)
     return nut

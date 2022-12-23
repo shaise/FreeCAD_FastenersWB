69f0b446ca64c2d22c146be8e4b7a668d41f0665
alexneufeld <alex.d.neufeld@gmail.com>
2022-12-22 21:17:51 -0700

    Refactor pan head screw creation
    - New method makeFlangedPanHeadScrew. this is only used for DIN 967
      screws as of now.
    - Add separate fastener data for ISO 14583 screws.

---
+++ FsFunctions/FSmakeFlangedPanHeadScrew.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+***************************************************************************
+*   Copyright (c) 2013, 2014, 2015                                        *
+*   Original code by:                                                     *
+*   Ulrich Brammer <ulrich1a[at]users.sourceforge.net>                    *
+*                                                                         *
+*   This file is a supplement to the FreeCAD CAx development system.      *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+*   as published by the Free Software Foundation; either version 2 of     *
+*   the License, or (at your option) any later version.                   *
+*   for detail see the LICENCE text file.                                 *
+*                                                                         *
+*   This software is distributed in the hope that it will be useful,      *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU Library General Public License for more details.                  *
+*                                                                         *
+*   You should have received a copy of the GNU Library General Public     *
+*   License along with this macro; if not, write to the Free Software     *
+*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+*   USA                                                                   *
+*                                                                         *
+***************************************************************************
+"""
+from screw_maker import *
+import FastenerBase
+
+
+def makeFlangedPanHeadScrew(self, fa):
+    """Create a pan head screw with a flange.
+
+    Supported types:
+    - DIN 967 cross recessed pan head Screw with collar
+    """
+    SType = fa.type
+    length = fa.calc_len
+    dia = self.getDia(fa.calc_diam, False)
+    if SType == 'DIN967':
+        P, b, c, da, dk, r, k, rf, x, cT, mH, mZ = fa.dimTable
+        alpha = math.acos((rf - k + c) / rf)
+        recess = self.makeHCrossRecess(cT, mH)
+        recess.translate(Base.Vector(0.0, 0.0, k))
+    # lay out fastener profile
+    fm = FastenerBase.FSFaceMaker()
+    fm.AddPoint(0.0, k)
+    fm.AddArc(
+        rf * math.sin(alpha / 2.0),
+        k - rf + rf * math.cos(alpha / 2.0),
+        rf * math.sin(alpha),
+        c
+    )
+    fm.AddPoint((dk) / 2.0, c)
+    fm.AddPoint((dk) / 2.0, 0.0)
+    fm.AddPoint(dia / 2.0 + r, 0.0)
+    fm.AddArc2(0.0, -r, 90)
+    if length - r > b:  # partially threaded fastener
+        thread_length = b
+        if not fa.thread:
+            fm.AddPoint(dia / 2, -1 * (length - b))
+    else:
+        thread_length = length - r
+    fm.AddPoint(dia / 2, -length)
+    fm.AddPoint(0.0, -length)
+    shape = self.RevolveZ(fm.GetFace())
+    shape = shape.cut(recess)
+    if fa.thread:
+        thread_cutter = self.CreateBlindThreadCutter(dia, P, thread_length)
+        thread_cutter.translate(
+            Base.Vector(0.0, 0.0, -1 * (length - thread_length))
+        )
+        shape = shape.cut(thread_cutter)
+    return shape

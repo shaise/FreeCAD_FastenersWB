5dbf205abbe7c7c3d29c1e9f1fe114726197683c
hasecilu <hasecilu@tuta.io>
2023-09-10 12:20:13 -0600

    Refactor T-slot nut BOM method.
    Since the data for DIN 508 was updated now it's present a single general case.

--- FastenerBase.py
+++ FastenerBase.py
@@ -906,14 +906,8 @@ class FSMakeBomCommand:
                          " Retaining Ring ") + obj.diameter, cnt)
 
     def AddTSlotNut(self, obj, cnt):
-        type = obj.type
-
-        if type[:3] == "DIN":
-            self.AddFastener(translate("FastenerBase", "T Slot Nut ") +
-                                obj.type + " " + obj.diameter, cnt)
-        elif type[:2] == "GN":
-            self.AddFastener(translate("FastenerBase", "T Slot Nut ") +
-                            obj.type + "-" + obj.slotWidth + "-" + obj.diameter, cnt)
+        self.AddFastener(translate("FastenerBase", "T Slot Nut ") +
+                        obj.type + "-" + obj.slotWidth + "-" + obj.diameter, cnt)
 
     def IsActive(self):
         return Gui.ActiveDocument is not None

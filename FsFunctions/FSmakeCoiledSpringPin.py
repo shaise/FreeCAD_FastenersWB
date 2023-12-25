943c4190aa6748b2b154e895a6357063c1e4221e
Alex Neufeld <alex.d.neufeld@gmail.com>
2023-12-24 23:16:04 -0700

    Fix error in coiled spring pins
    The lack of a call to the toShape() method before performing the helical
    sweep causes an error in some versions of FreeCAD.
    
    Also added some more comments to this file.

--- FsFunctions/FSmakeCoiledSpringPin.py
+++ FsFunctions/FSmakeCoiledSpringPin.py
@@ -44,7 +44,7 @@ def makeCoiledSpringPin(self, fa):
     fm.AddPoint(d_1 / 2 - s, a-length)
     # create a swept spiral profile
     # NOTE: counter-intuitively, during testing, adding MORE reference points to the
-    # spline increaased the performance of the swept solid, while also eliminating
+    # spline increased the performance of the swept solid, while also eliminating
     # degenerate results for some sizes. Don't reduce r and expect a simple perf boost!
     r = 100
     turns = 2.25
@@ -55,13 +55,24 @@ def makeCoiledSpringPin(self, fa):
         # transformed in unexpected ways during the sweep process
         points.append(
             Base.Vector(
+                # multiply by 1.01 to avoid self-intersecting faces in the spiral
+                #            |
+                #            v
                 (d_2 / 2 - 1.01 * s * i / r) * math.cos(-i / r * 2 * math.pi),
                 (d_2 / 2 - 1.01 * s * i / r) * math.sin(-i / r * 2 * math.pi),
                 0.0,
             )
         )
+    # ref: https://forum.freecad.org/viewtopic.php?t=47282
+    # Part.BSplineCurve([poles],              #[vector]
+    #                 [multiplicities],     #[int]
+    #                 [knots],              #[float]
+    #                 periodic,             #bool
+    #                 degree,               #int
+    #                 [weights],            #[float]
+    #                 checkRational)        #bool
     spiral = Part.BSplineCurve(points, None, None, False, 3, None, False)
-    sweep = Part.BRepOffsetAPI.MakePipeShell(Part.Wire([spiral]))
+    sweep = Part.BRepOffsetAPI.MakePipeShell(Part.Wire([spiral.toShape()]))
     sweep.setFrenetMode(True)
     sweep.add(fm.GetClosedWire())
     if sweep.isReady():

6b541e19b61683ad1f33484a804d380b54238a22
Roy-043 <70520633+Roy-043@users.noreply.github.com>
2024-05-21 21:37:49 +0200

    Update FastenerBase.py

--- FastenerBase.py
+++ FastenerBase.py
@@ -567,19 +567,19 @@ def PositionDone(center, radius, done_list, tol=1e-6):
     return False
 
 
-def FSGetAttachableSelections():
+def FSGetAttachableSelections(screwObj=None):
     asels = []
-    for selObj in Gui.Selection.getSelectionEx():
+    for selObj in Gui.Selection.getSelectionEx("", 0):
+        if screwObj is not None and selObj.Object == screwObj:
+            continue
+
         baseObjectNames = selObj.SubElementNames
         obj = selObj.Object
-        grp = obj.getParentGeoFeatureGroup()
-        if grp is not None and hasattr(grp, "TypeId") and grp.TypeId == "PartDesign::Body":
-            obj = grp
         position_done_list = []  # list with sublists to store the center and radius
         # of processed edges to avoid duplicate fasteners
 
         for baseObjectName in baseObjectNames:
-            shape = obj.Shape.getElement(baseObjectName)
+            shape = obj.getSubObject(baseObjectName)
 
             # add explicitly selected edges
             if hasattr(shape, "Curve"):
@@ -767,7 +767,7 @@ class FSMoveCommand:
             obj = selObj.Object
             if hasattr(obj, 'Proxy') and isinstance(obj.Proxy, FSBaseObject):
                 screwObj = obj
-        aselects = FSGetAttachableSelections()
+        aselects = FSGetAttachableSelections(screwObj)
         if len(aselects) > 0:
             edgeObj = aselects[0]
         return screwObj, edgeObj

0a6c9ce94acea077580c7ac03874dada84bc6c35
Yuri D'Elia <wavexx@thregr.org>
2024-11-25 22:52:50 +0100

    Fix Thread cache invalidation
    Fix typo leading to missing cache invalidation when changing thread
    generation settings.

--- FastenerBase.py
+++ FastenerBase.py
@@ -268,7 +268,7 @@ def FSGetKey(*args):
 # removes all cached fasteners with real thread
 def FSCacheRemoveThreaded():
     for key in list(FSCache.keys()):
-        if key.find('thread:True') > 0:
+        if key.find('Thread:True') > 0:
             FreeCAD.Console.PrintLog("Removing cached shape: " + key + "\n")
             del FSCache[key]
 
